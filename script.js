2e3bed86743213488140fa2a6c863f8278ae115c
Curtis Myers <curt.myers@aol.com>
2021-04-10 22:27:33 -0400

    Created guitar web page in HTML, CSS, Bootstrap, Jquery

---
+++ script.js
@@ -0,0 +1,19 @@
+
+$(".navbar-toggler").click(function(){
+    sound.play();
+   });
+$(".navbar-toggler").click(function(){
+    $(".guitar-icon").toggleClass("flip");
+    $(".background").fadeOut();  
+    $(".background").fadeIn();
+});
+$(".bi-github").mouseover(function(){
+    $(this).fadeOut();
+    $(this).fadeIn();
+});
+$(".bi-github").mouseleave(function(){
+    $(this).fadeIn();
+    });
+    $(".navbar-toggler").click(function(){
+        sound.play();
+    });
